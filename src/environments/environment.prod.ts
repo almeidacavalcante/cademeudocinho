072dbeed83793e7353ecd7071821ddee6fd49439
almeidacavalcante <jalmeidacn@gmail.com>
2018-03-12 23:50:46 -0300

    First commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCwvSATQ0JHYBxzyQnWK_QSE5sIwHLhS_o",
+    authDomain: "cademeudocinho-app.firebaseapp.com",
+    databaseURL: "https://cademeudocinho-app.firebaseio.com",
+    projectId: "cademeudocinho-app",
+    storageBucket: "cademeudocinho-app.appspot.com",
+    messagingSenderId: "395162281392"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCwvSATQ0JHYBxzyQnWK_QSE5sIwHLhS_o",
+    authDomain: "cademeudocinho-app.firebaseapp.com",
+    databaseURL: "https://cademeudocinho-app.firebaseio.com",
+    projectId: "cademeudocinho-app",
+    storageBucket: "cademeudocinho-app.appspot.com",
+    messagingSenderId: "395162281392"
+  }
+};
